26f905b13ec4304d8f1dd0a5402e509e9085f917
DEMKOM58 <demkom58@gmail.com>
2020-03-09 04:39:39 +0200

    Added working math

--- src/main/java/com/demkom58/nmlab1/MainController.java
+++ src/main/java/com/demkom58/nmlab1/MainController.java
@@ -2,34 +2,237 @@ package com.demkom58.nmlab1;
 
 import com.demkom58.divine.gui.GuiController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
+import org.jetbrains.annotations.NotNull;
+import org.mariuszgromada.math.mxparser.Expression;
+import org.mariuszgromada.math.mxparser.Function;
 
 public class MainController extends GuiController {
 
-    @FXML private TextField functionInput;
-    @FXML private TextField fromAInput;
-    @FXML private TextField toBInput;
-    @FXML private TextField accuracyInput;
+    @FXML
+    private TextField functionInput;
+    @FXML
+    private TextField fromAInput;
+    @FXML
+    private TextField toBInput;
+    @FXML
+    private TextField accuracyInput;
+
+    private Function function;
+    private Expression expression;
+    private double aInterval;
+    private double bInterval;
+    private double precision;
+
+    private double a;
+    private double b;
+
+    private int iteration = 0;
 
     @Override
     public void init() {
         super.init();
+        read();
     }
 
     @FXML
     public void bisection(MouseEvent event) {
+        try {
+            check();
+        } catch (Exception e) {
+            showErrorMessage(e);
+            return;
+        }
 
+        double fa = function.calculate(a);
+        double x = (a + b) / 2;
+
+        if ((fa * function.calculate(x)) < 0)
+            this.b = x;
+        else
+            this.a = x;
+
+        if (Math.abs(b - a) <= precision) {
+            showInfoMessage("Результат знайдений.", "Результат з точністю " + precision + " дорівнює " + x);
+            read();
+            return;
+        }
+
+        showInfoMessage(
+                "Ітерація: " + iteration + ".",
+                "Орієнтовний корінь: " + x + "."
+        );
+
+        iteration++;
     }
 
     @FXML
     public void secant(MouseEvent event) {
+        try {
+            check();
+        } catch (Exception e) {
+            showErrorMessage(e);
+            return;
+        }
 
+        double fa = function.calculate(a);
+        double fb = function.calculate(b);
+        double m = (fb - fa) / (b - a);
+        double diferencia;
+
+        double x = ((-fa + m * a) / m);
+        if ((fa * function.calculate(x)) < 0) {
+            diferencia = Math.abs(b - x);
+            b = x;
+        } else {
+            diferencia = Math.abs(a - x);
+            a = x;
+        }
+
+        if (((function.calculate(x)) <= precision) && (diferencia <= precision)) {
+            showInfoMessage("Результат знайдений.", "Результат з точністю " + precision + " дорівнює " + x);
+            read();
+            return;
+        }
+
+        showInfoMessage(
+                "Ітерація: " + iteration + ".",
+                "Орієнтовний корінь: " + x + "."
+        );
+
+        iteration++;
     }
 
     @FXML
     public void tanget(MouseEvent event) {
+        try {
+            check();
+        } catch (Exception e) {
+            showErrorMessage(e);
+            return;
+        }
 
+        final double fa = function.calculate(a);
+        final Expression ex = new Expression(
+                "der(" + expression.getExpressionString() + ", x, " + a + ")"
+        );
+
+        final double calculate = ex.calculate();
+        if (calculate == 0) {
+            showErrorMessage("Помилка. Ділення на нуль.");
+            read();
+            return;
+        }
+
+        b = a - (fa / calculate);
+        if ((Math.abs(b - a) <= precision)) {
+            showInfoMessage("Результат знайдений.", "Результат з точністю " + precision + " дорівнює " + b);
+            read();
+            return;
+        }
+
+        showInfoMessage(
+                "Ітерація: " + iteration + ".",
+                "Орієнтовний корінь: " + b + "."
+        );
+
+        a = b;
+        iteration++;
+    }
+
+    @FXML
+    public void onChanged(KeyEvent event) {
+        read();
+    }
+
+    private void check() throws IllegalStateException {
+        if (!function.checkSyntax())
+            throw new IllegalStateException("Перевірте введену функцію.\n" + function.getErrorMessage());
+
+        if (aInterval == Double.MIN_VALUE)
+            throw new IllegalStateException("Перевірте введені дані для змінної 'a'.");
+
+        if (bInterval == Double.MIN_VALUE)
+            throw new IllegalStateException("Перевірте введені дані для змінної 'b'.");
+
+        if (precision == Double.MIN_VALUE)
+            throw new IllegalStateException("Перевірте задану точність.");
+    }
+
+    private void read() {
+        iteration = 0;
+
+        String functionText = functionInput.getText();
+        if (functionText.isBlank()) {
+            final String promptText = functionInput.getPromptText();
+            functionText = promptText.substring(promptText.indexOf("f(x) = "));
+        }
+
+        function = new Function(functionText);
+        expression = new Expression(functionText.substring("f(x) = ".length()));
+
+        final String aIntervalText = fromAInput.getText();
+
+        try {
+            aInterval = aIntervalText.isBlank()
+                    ? Double.parseDouble(fromAInput.getPromptText())
+                    : Double.parseDouble(aIntervalText);
+        } catch (NumberFormatException e) {
+            aInterval = Double.MIN_VALUE;
+            return;
+        }
+
+        final String bIntervalText = fromAInput.getText();
+        try {
+            bInterval = bIntervalText.isBlank()
+                    ? Double.parseDouble(toBInput.getPromptText())
+                    : Double.parseDouble(bIntervalText);
+        } catch (NumberFormatException e) {
+            bInterval = Double.MIN_VALUE;
+            return;
+        }
+
+        final String precisionText = accuracyInput.getText();
+        try {
+            precision = precisionText.isBlank()
+                    ? Double.parseDouble(accuracyInput.getPromptText())
+                    : Double.parseDouble(precisionText);
+
+            if (precision <= 0) {
+                precision = 0.1;
+            }
+        } catch (NumberFormatException e) {
+            precision = Double.MIN_VALUE;
+            return;
+        }
+
+
+        a = aInterval;
+        b = bInterval;
+    }
+
+    private void showErrorMessage(@NotNull final Exception exception) {
+        showErrorMessage(exception.getMessage());
+    }
+
+    private void showErrorMessage(@NotNull final String error) {
+        alert(Alert.AlertType.ERROR, "Помилка", error, ButtonType.OK);
+    }
+
+    private void showInfoMessage(@NotNull final String header, @NotNull final String info) {
+        alert(Alert.AlertType.INFORMATION, header, info, ButtonType.OK);
+    }
+
+    private void alert(Alert.AlertType type, String header, String message, ButtonType... types) {
+        Alert alert = new Alert(type, message, types);
+        alert.setHeaderText(header);
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.show();
     }
 
 }
